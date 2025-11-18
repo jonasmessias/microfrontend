a6f7300a71bc2667ac4d37ba65e8b9c20c94c8be
Jonas <jonasmessias30@gmail.com>
2025-11-18 01:46:01 -0300

    feat(shell): add application entry points and type declarations

---
+++ packages/shell/src/types/module-federation.d.ts
@@ -0,0 +1,18 @@
+declare module 'mfeShop/Shop' {
+  const Shop: React.ComponentType;
+  export default Shop;
+}
+
+declare module 'mfeProducts/Products' {
+  const Products: React.ComponentType;
+  export default Products;
+}
+
+declare module 'mfeCart/Cart' {
+  const Cart: React.ComponentType;
+  export default Cart;
+}
+
+declare module 'mfeCart/cartStore' {
+  export const useCartStore: any;
+}
